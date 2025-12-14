cbf1dafb7f32affe63e9d84d6b6278872e33f26e
argiedio121180 <argiedio121180@gmail.com>
2025-12-14 17:24:07 +0800

    feat: landing page

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="min-h-screen flex flex-col justify-center items-center bg-black">
+       
+
+
+
+        <h1 class="text-3xl text-white italic mb-5">WELCOME TO THE LAND OF THE BRAVE</h1>
+      
+
+        
+        
+      <div class="border-2 border-red-500 p-4 rounded-md text-white">
+        This is a red bordered box
+    </div>
+
+
+    </div>
+</template>
